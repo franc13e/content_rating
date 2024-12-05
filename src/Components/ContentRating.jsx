3be850076a7820f92483da38bb2f1d24ffc34f1d
Francine <francinefaith03@gmail.com>
2024-12-05 12:07:08 -0500

    Added total ratings feature

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -7,15 +7,18 @@ class ContentRating extends Component {
         this.state = {
             likes: 0,
             dislikes: 0,
+            totalRatings:0,
             handleLike:() => {
                 this.setState((prevState) => ({
-                    likes: prevState.likes + 1
+                    likes: prevState.likes + 1,
+                    totalRatings: prevState.totalRatings +1
                 }));
             },
 
             handleDislike:() => {
                 this.setState((prevState) => ({
-                    dislikes: prevState.dislikes +1
+                    dislikes: prevState.dislikes +1,
+                    totalRatings: prevState.totalRatings +1
                 }));
             }
         };
@@ -58,6 +61,7 @@ I know your favourite colour, but do you even know mine?
                     Dislike ({this.state.dislikes})
                 </button>
             </div>
+            <p>Total Ratings: {this.state.totalRatings}</p>
             </div>
             </>
         )
